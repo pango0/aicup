3211c9cb0b1b0960fb5fc956c874faf2e24b2e64
jingjin412 <b10204022@g.ntu.edu.tw>
2025-06-15 00:17:30 +0800

    Update preprocess(to_alpaca).py

--- all final code/phase2/preprocess(to_alpaca).py
+++ all final code/phase2/preprocess(to_alpaca).py
@@ -1,7 +1,7 @@
 import json
 import pandas as pd
 
-data = pd.read_csv("../phase1/task1_answer.txt", sep='\t',header=None, names=["id", "text"])
+data = pd.read_csv("../phase1/task1_zh_answer.txt", sep='\t',header=None, names=["id", "text"])
 
 alpaca_data = []
 for _, row in data.iterrows():
@@ -10,5 +10,5 @@ for _, row in data.iterrows():
         "instruction": row["text"]
     })
 
-with open("task1_alpaca.json", "w") as f:
+with open("task1_zh_alpaca.json", "w") as f:
     json.dump(alpaca_data, f, ensure_ascii=False, indent=2)
